4cc764141cc83c85e9d1834780f489eb3bcf8d36
OrangeAed <ajsimonsrudolph@gmail.com>
2025-04-16 02:25:47 +0000

    Part 1 step 2

--- proc.c
+++ proc.c
@@ -6,6 +6,7 @@
 #include "x86.h"
 #include "proc.h"
 #include "spinlock.h"
+#include "pstat.h"
 
 struct {
   struct spinlock lock;
@@ -138,6 +139,8 @@ userinit(void)
   p->tf->eflags = FL_IF;
   p->tf->esp = PGSIZE;
   p->tf->eip = 0;  // beginning of initcode.S
+  p->ticks = 0;
+  p->tickets = 10;
 
   safestrcpy(p->name, "initcode", sizeof(p->name));
   p->cwd = namei("/");
